6263864fb5122dc3edd70b98a5e0501cbaa08b4d
Danny <luuthanhdannydavidnguyen@gmail.com>
2020-05-27 07:52:20 -0400

    add basic mod files

---
+++ src/main/java/danny8208/autotorch/AutoTorch.java
@@ -0,0 +1,43 @@
+package danny8208.autotorch;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("autotorch")
+public class AutoTorch {
+    public static final String MODID = "autotorch";
+    public static final CreativeGroup GROUP = new CreativeGroup();
+    public static final Logger logger = LogManager.getLogger();
+
+    public AutoTorch() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents {
+        @SubscribeEvent
+        public static void onItemsRegistry(final RegistryEvent.Register<Item> event) {
+            event.getRegistry().registerAll(
+            );
+        }
+
+        @SubscribeEvent
+        public static void onBlocksRegistry(final RegistryEvent.Register<Block> event) {
+            event.getRegistry().registerAll(
+            );
+        }
+    }
+}
